e6e5177775ad2fc856b11d9e43c42232e0d372c1
gaurangbhatt19 <gaurangbhatt19@gmail.com>
2021-06-09 00:41:45 +0530

    2 files modified

--- app/hello-framework/hello-framework.component.ts
+++ app/hello-framework/hello-framework.component.ts
@@ -3,13 +3,6 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 @Component({
   selector: 'hello-framework',
   styleUrls: ['./hello-framework.component.scss'],
-  templateUrl: './hello-framework.component.html',
+  templateUrl: './hello-framework.component.html'
 })
-export class HelloFrameworkComponent {
-  @Input()
-  name: string;
-  @Input()
-  version: number;
-  @Output()
-  readonly release: EventEmitter<void> = new EventEmitter();
-}
+export class HelloFrameworkComponent {}
